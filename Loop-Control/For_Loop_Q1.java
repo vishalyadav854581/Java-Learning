71c76c93d35ccfcfa46d805e269fe9b243bb74db
vishalyadav854581 <vishalyadav854581@gmail.com>
2026-07-05 04:57:37 +0000

    Added For_Loop_Q1 program

---
+++ Loop-Control/For_Loop_Q1.java
@@ -0,0 +1,9 @@
+public class For_Loop_Q1 {
+    public static void main (String [] args) {
+
+        for(int i = 1 ; i <= 5 ; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
---
+++ Loop-Control/For_Loop_Q2.java
@@ -0,0 +1,10 @@
+public class For_Loop_Q2 {
+    public static void main (String [] args) {
+
+        for(int i = 10 ; i>= 1 ; i--)
+        {
+            System.out.println(i);
+        }
+    }
+    
+}
