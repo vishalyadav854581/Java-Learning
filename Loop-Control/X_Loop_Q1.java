185928d59909ca1403f993aa2541bc9394ab8a36
vishalyadav854581 <vishalyadav854581@gmail.com>
2026-07-09 04:39:04 +0000

    Added Java program

---
+++ Loop-Control/X_Loop_Q1.java
@@ -0,0 +1,22 @@
+ import java.util.*;
+ public class X_Loop_Q1 {
+     public static void main (String [] args){
+         
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the Number : ");
+int number = sc.nextInt();
+
+int temp = number ;
+int result = 0;
+int rem ;
+ 
+  while(number !=0)
+ {
+     rem = number % 10 ;
+     result=result*10 +rem;
+     number = number /10 ;
+}
+  System.out.println("Reverse of the Number is : "+result);
+ 
+     }
+ }
