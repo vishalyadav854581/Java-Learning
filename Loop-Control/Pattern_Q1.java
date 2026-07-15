25efc0f8dee0a5a67fd116944b8aace3074a3981
vishalyadav854581 <vishalyadav854581@gmail.com>
2026-07-15 07:53:42 +0000

    Initial commit

---
+++ Loop-Control/Pattern_Q1.java
@@ -0,0 +1,18 @@
+/*Solid Square  */
+
+public class Pattern_Q1{
+    public static void main(String [] args){
+
+        int m = 4 ;
+        int n = 5 ;
+
+for(int i = 1 ; i<=m ; i++)
+{
+    for(int j = 1 ; j<=n ; j++)
+    {
+        System.out.print("*");
+    }
+    System.out.println("");
+}
+    }
+}
---
+++ Loop-Control/Pattern_Q4.java
@@ -0,0 +1,16 @@
+ public class Pattern_Q4 {
+    public static void main(String[] args) {
+
+        int m = 4;
+
+        for(int i = m; i >= 1; i--)
+        {
+            for(int j = 1; j <= i; j++)
+            {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
